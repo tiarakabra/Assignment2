df5ffeb97329ee62c1b460659ed3312c804e19a0
tiarakabra <136440241+tiarakabra@users.noreply.github.com>
2023-06-17 11:22:32 +0530

    Add files via upload

---
+++ Operators.py
@@ -0,0 +1,60 @@
+a=6
+b=9
+#arithmatic operators
+print("Addition",a+b)
+print("Subtraction",a-b)
+print("Multiplication",a*b)
+print("Division",a/b)
+print("Modulo",a%b)
+print("Floor Division",a//b)
+print("Exponentieal",a**b)
+
+#assignment operators
+a+=1
+print("+=",a)
+a-=1
+print("-=",a)
+a*=1
+print("*=",a)
+a/=1
+print("/=",a)
+a%=1
+print("%=",a)
+
+#Comparison operators
+print("==",a==b)
+print("!=",a!=b)
+print(">",a>b)
+print("<",a<b)
+print(">=",a>=b)
+print("<=",a<=b)
+
+#Logical operators
+c=True
+d=False
+print(c and d)
+print(c or d)
+print(not b)
+
+#Bitwise operators
+x = 10
+y = 4
+print("Bitwise and",x & y)
+print("Bitwise or",x|y)
+print("Bitwise xor",x^y)
+print("Bitwise left shift",x<<y)
+print("Bitwse right shift",x>>y)
+
+#Identity Operators
+e="Python"
+print('P'is e)
+print('fo' is not e)
+
+#Membership operators
+p = 14
+q = 16
+list = [1, 16, 3, 14, 5 ]
+print(p in list)
+print(q in list)
+
+
